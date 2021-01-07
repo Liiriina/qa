92a7cfae3d366adc6032f91881347215217d1df0
Irina <liiriina@gmail.com>
2021-01-07 20:09:26 +0200

    Change numbers in project

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -6,7 +6,7 @@ public class MyFirstTest {
     public void firstTest (){
 
         System.out.println("Hello, Corona!");
-       int sum =  sumTwoDigits(15,45);
+       int sum =  sumTwoDigits(11,45);
         System.out.println("And the sum is: " + sum);
         System.out.println(sumTwoDigits(9,13));
         System.out.println(name);
