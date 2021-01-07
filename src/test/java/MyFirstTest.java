de717057ea011237c0c4fc27b73e2f713e1cad2a
Irina <liiriina@gmail.com>
2021-01-07 19:25:41 +0200

    Initial commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+    private String name = "Carl";
+    @Test
+    public void firstTest (){
+
+        System.out.println("Hello, Corona!");
+       int sum =  sumTwoDigits(15,45);
+        System.out.println("And the sum is: " + sum);
+        System.out.println(sumTwoDigits(9,13));
+        System.out.println(name);
+    }
+private int sumTwoDigits (int a, int b){
+       int c = a + b;
+       return c;
+}
+
+}
